78e37c493300f19d5fcde36ce6dc36559675e1ac
Spasimir Kirov <spasimirkirov@gmail.com>
2020-08-19 05:56:29 +0300

    Cleanups and restyle

--- inc/template/create.php
+++ inc/template/create.php
@@ -17,18 +17,6 @@ if (isset($_POST['submit_term_link'])) {
         show_message('<div class="error notice notice-error is-dismissible"><p>Не сте посочили мета атрибут</p></div>');
 }
 
-if (isset($_POST['submit_term_action'])) {
-    $requestApi = new \WooCustomAttributes\Inc\Request();
-    if ($_POST['action'] === 'none') {
-        show_message('<div class="error notice notice-error is-dismissible"><p>Не сте посочили действие</p></div>');
-    }
-    if ($_POST['action'] === 'delete') {
-        (!isset($_POST['relation_ids']) || empty($_POST['relation_ids'])) ?
-            show_message('<div class="error notice notice-error is-dismissible"><p>Не сте посочили релации за изтриване</p></div>') :
-            $requestApi->delete_relation($_POST['relation_ids']);
-    }
-}
-
 $db = new Database();
 $available_taxonomies = wc_get_attribute_taxonomies();
 $available_meta_attributes = WooCustomAttributes\Inc\Api::list_distinct_meta_attributes();
@@ -39,62 +27,58 @@ sort($available_meta_attributes);
 <div class="container my-2">
     <div class="row">
         <div class="col-12">
-            <nav>
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= admin_url('admin.php?page=woo_custom_attributes'); ?>">Релации</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active"
-                           href="<?= admin_url('admin.php?page=woo_custom_attributes_create'); ?>">Добави</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= admin_url('admin.php?page=woo_custom_attributes_settings'); ?>">Настройки</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <div class="col-12 card-header bg-dark text-light">
-            Създаване на релация меджу Атрибут и Мета атрибут
-        </div>
-        <div class="col border rounded ">
-            <label for="select_taxonomy">Избор на таксономия на атрибут</label>
-            <div class="input-group">
-                <select class="custom-select" id="select_taxonomy" name="taxonomy_id">
-                    <option value="none">Избор</option>
-                    <?php foreach ($available_taxonomies as $taxonomy): ?>
-                        <option value="<?php echo $taxonomy->attribute_id; ?>">
-                            <?php echo $taxonomy->attribute_label ?> </option>
-                    <?php endforeach; ?>
-                </select>
-                <div class="input-group-append">
-                    <input class="btn btn-primary btn-sm" name="submit_term_link" type="submit"
-                           value="Свързване">
-                </div>
+            <ul class="nav nav-tabs p-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= admin_url('admin.php?page=woo_custom_attributes'); ?>">Релации</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active"
+                       href="<?= admin_url('admin.php?page=woo_custom_attributes_create'); ?>">Добави</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= admin_url('admin.php?page=woo_custom_attributes_settings'); ?>">Настройки</a>
+                </li>
+            </ul>
+            <div class="card-header bg-dark text-light">
+                Създаване на релация меджу Атрибут и Мета атрибут
             </div>
-            <p class="text-secondary">
-                Изберете с коя таксономия да бъдат свързани мета атрибутите.<br>
-                За създаване на таксономии, вижте
-                <a href="<?= admin_url('edit.php?post_type=product&page=product_attributes') ?>">
-                    Продукти->Атрибути
-                </a>
-            </p>
-            <form action="" method="post">
-                <div class="p-2">
-                    <label for="select_meta">Избор на мета атрибути</label>
-                    <div class="form-row">
-                        <select class="custom-select custom-select-sm" id="select_meta" multiple="multiple"
-                                name="meta_name[]" size="20">
-                            <option value="0">Избор</option>
-                            <?php foreach (array_filter($available_meta_attributes) as $attribute): ?>
-                                <option value="<?php echo $attribute; ?>">
-                                    <?php echo $attribute ?> </option>
-                            <?php endforeach; ?>
-                        </select>
+            <div class="p-2">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="select_meta">Избор на мета атрибути</label>
+                        <div class="form-row">
+                            <select class="form-control" id="select_meta" multiple name="meta_name[]" size="20">
+                                <option value="0">Избор</option>
+                                <?php foreach (array_filter($available_meta_attributes) as $attribute): ?>
+                                    <option value="<?php echo $attribute; ?>">
+                                        <?php echo $attribute ?> </option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <p>Задръжте "CTR" бутона повреме на избора си за селектиране на няколко опции. </p>
                     </div>
-                    <p>Задръжте "CTR" бутона повреме на избора си за селектиране на няколко опции. </p>
-                </div>
-            </form>
+
+                    <label for="select_taxonomy">Избор на таксономия на атрибут</label>
+                    <p class="text-secondary">
+                        Изберете с коя таксономия да бъдат свързани мета атрибутите.<br>
+                        За създаване на таксономии, вижте
+                        <a href="<?= admin_url('edit.php?post_type=product&page=product_attributes') ?>">
+                            Продукти->Атрибути
+                        </a>
+                    </p>
+                    <select class="custom-select" id="select_taxonomy" name="taxonomy_id">
+                        <option value="none">Избор</option>
+                        <?php foreach ($available_taxonomies as $taxonomy): ?>
+                            <option value="<?php echo $taxonomy->attribute_id; ?>">
+                                <?php echo $taxonomy->attribute_label ?> </option>
+                        <?php endforeach; ?>
+                    </select>
+
+                    <input class="btn btn-primary btn-sm ml-2" name="submit_term_link" type="submit"
+                           value="Свързване">
+                </form>
+            </div>
         </div>
     </div>
+</div>
 </div>

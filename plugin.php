aa3256ea94d24b379c61cb97c2d00b3f40ad3009
Spasimir Kirov <spasimirkirov@gmail.com>
2020-08-24 15:33:26 +0300

    Added woo-product-attributes plugin as dependency for the plugin

--- plugin.php
+++ plugin.php
@@ -13,30 +13,36 @@ Text Domain: woo-attribute-generator
 if (!defined('ABSPATH')) {
     die;
 }
-define('BASE_PATH', plugin_dir_path(__FILE__));
-define('BASE_URL', plugin_dir_url(__FILE__));
-
-// include the Composer autoload file
-require BASE_PATH . 'vendor/autoload.php';
 
 use WooCustomAttributes\Inc\Hooks;
 use WooCustomAttributes\Inc\WooAttributePlugin;
 
-$hooks = new Hooks();
-register_activation_hook(__FILE__, [$hooks, 'activate']);
-register_deactivation_hook(__FILE__, [$hooks, 'deactivate']);
-register_uninstall_hook(__FILE__, [$hooks, 'uninstall']);
+$dependencies = [
+    'woocommerce/woocommerce.php' => 'WooCommerce',
+    'woo-product-attributes/plugin.php' => 'Woo Product Attributes',
+];
 
-$is_woo_active = in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')));
-if (!$is_woo_active) {
-    add_action('admin_notices', function () {
-        ?>
-        <div class="notice notice-warning is-dismissible">
-            <p><?php _e('WooCommerce е неактивен, моля активирайте го преди да използвате Woo Attribute Generator'); ?></p>
-        </div>
-        <?php
-    });
-} else {
+$errors = 0;
+foreach ($dependencies as $plugin_file => $plugin_name) {
+    $is_plugin_active = in_array($plugin_file, apply_filters('active_plugins', get_option('active_plugins')));
+    if (!$is_plugin_active) {
+        add_action('admin_notices', function () use ($plugin_name) {
+            ?>
+            <div class="notice notice-warning is-dismissible">
+                <p><?php _e('' . $plugin_name . ' е неактивен, моля активирайте го преди да използвате Woo Custom Attributes'); ?></p>
+            </div>
+            <?php
+        });
+        $errors++;
+    }
+}
+if ($errors === 0) {
+    // include the Composer autoload file
+    require plugin_dir_path(__FILE__) . 'vendor/autoload.php';
+    $hooks = new Hooks();
+    register_activation_hook(__FILE__, [$hooks, 'activate']);
+    register_deactivation_hook(__FILE__, [$hooks, 'deactivate']);
+    register_uninstall_hook(__FILE__, [$hooks, 'uninstall']);
     $WooAttributePlugin = new WooAttributePlugin();
     $WooAttributePlugin->init();
 }
